d2de8c88b51d8cc70ad219b5229a4bfca1103584
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-01-18 16:58:37 -0300

    Converted project to React.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+import Destinations from "./Destinations";
+
+function App() {
+    return (
+        <>
+            <Banner />
+            <Destinations />
+        </>
+    );
+}
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,51 @@
+function Banner() {
+    return (
+        <div class="flex bg-gray-100">
+            <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:w-1/2 lg:max-w-full lg:py-24 lg:px-10">
+                <div class="xl:max-w-lg xl:ml-auto">
+                    <img
+                        class="h-12"
+                        src="img/workcation-logo.svg"
+                        alt="Workcation logo"
+                    />
+
+                    <img
+                        class="mt-6 rounded-lg shadow-lg sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
+                        src="img/workcationing.jpg"
+                        alt="Man workcationing on the beach"
+                    />
+                    <h1 class="mt-6 text-2xl font-bold text-gray-800 leading-tight sm:mt-8 sm:text-3xl lg:text-3xl xl:text-4xl">
+                        You can work from anywhere.
+                        <br class="hidden sm:inline" />
+                        <span class="text-blue-600">Take advantage of it.</span>
+                    </h1>
+                    <p class="mt-2 text-gray-600 font-medium sm:mt-4 sm:text-xl">
+                        <span id="width"></span>&nbsp; Workcation helps you find
+                        work places work-friendly places so you can relax but
+                        keep working on what's important
+                    </p>
+                    <div class="mt-6">
+                        <a class="btn-blue shadow-lg sm:text-base" href="#">
+                            Book your escape
+                        </a>
+                        <a
+                            class="btn-gray ml-2 shadow-lg sm:text-base"
+                            href="#"
+                        >
+                            Learn more
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden lg:block lg:w-1/2 lg:relative">
+                <img
+                    class="absolute inset-0 h-full w-full object-cover object-center"
+                    src="img/workcationing.jpg"
+                    alt="Man workcationing on the beach"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
