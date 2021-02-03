731fcdbbb97efcc4290aae5f5b286b93f668d303
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-02-03 10:44:11 -0300

    Extended default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
@@ -5,6 +7,17 @@ module.exports = {
         extend: {
             colors: {
                 "brand-blue": "#1992d4",
+                blueGray: colors.blueGray,
+                orange: colors.orange,
+                amber: colors.amber,
+                lime: colors.lime,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                violet: colors.violet,
+                fuchsia: colors.fuchsia,
+                pink: colors.pink,
+                rose: colors.rose,
             },
             spacing: {
                 128: "32rem",
