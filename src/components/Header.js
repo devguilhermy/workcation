f86a9e641882d8d2a56ff80582dbc50db6377632
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-02-13 01:04:55 -0300

    Added dropdown alternative to mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,45 @@ import { useState } from "react";
 import ProfileDropdown from "./ProfileDropdown";
 
 export default function Header() {
+    const [openMenu, setOpenMenu] = useState(false);
+
     const links = [
         { name: "Home", icon: "", href: "#" },
         { name: "Cities", icon: "", href: "#" },
         { name: "Help", icon: "", href: "#" },
     ];
 
-    const [openMenu, setOpenMenu] = useState(false);
+    const profileLinks = [
+        {
+            name: "Hires",
+            location: "#",
+            icon: "",
+        },
+        {
+            name: "My properties",
+            location: "#",
+            icon: "",
+        },
+        {
+            name: "Settings",
+            location: "#",
+            icon: "",
+        },
+        {
+            name: "Support",
+            location: "#",
+            icon: "",
+        },
+        {
+            name: "Sign out",
+            location: "#",
+            icon: "",
+        },
+    ];
 
     return (
-        <header className="relative bg-gray-300 shadow-md sm:flex sm:justify-between sm:items-center sm:px-10 sm:py-3 z-10">
-            <div className="flex items-center justify-between px-5 py-3 sm:p-0">
+        <header className="relative bg-gray-300 shadow-md sm:flex sm:justify-between sm:px-10 sm:py-3 z-10">
+            <div className="flex justify-between px-5 py-3 sm:p-0">
                 <div className="">
                     <img
                         class="h-10"
@@ -52,22 +80,45 @@ export default function Header() {
                     </button>
                 </div>
             </div>
-            <div
-                className={`px-3 pt-4 pb-8 sm:p-0 justify-center items-center text-center sm:flex  ${
-                    openMenu ? "sm:block" : "hidden"
-                }`}
-            >
-                {links.map((link) => {
-                    return (
-                        <a
-                            href={link.href}
-                            className="mt-4 mx-auto block px-2 py-2 text-2xl rounded-full w-1/3 font-semibold text-gray-600  transition-all hover:text-gray-900 hover:bg-gray-400 focus:text-gray-900 focus:bg-gray-400 leading-none sm:ml-4 sm:mt-0 sm:text-xl"
-                        >
-                            {link.name}
-                        </a>
-                    );
-                })}
-                <ProfileDropdown className="ml-5" />
+            <div className={`sm:block ${openMenu ? "block" : "hidden"}`}>
+                <div class="px-4 py-4 sm:p-0 sm:flex ">
+                    {links.map((link) => {
+                        return (
+                            <a
+                                href={link.href}
+                                className="mt-4 block px-2 py-2 text-2xl rounded-full  font-semibold text-gray-600  transition-all hover:text-gray-900 hover:bg-gray-400 focus:text-gray-900 focus:bg-gray-400 leading-none sm:ml-4 sm:mt-0 sm:text-xl"
+                            >
+                                {link.name}
+                            </a>
+                        );
+                    })}
+                    <ProfileDropdown className="hidden sm:block sm:ml-5" />
+                </div>
+                <div className="pt-2 px-6 border-t border-gray-400 sm:hidden">
+                    <div className="mt-4 flex items-center">
+                        <img
+                            class="w-14 h-14 border-2 border-gray-400 rounded-full object-cover object-center"
+                            src="https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                            alt="Avatar"
+                        />
+                        <span className="ml-2 font-semibold text-xl">
+                            William Doe
+                        </span>
+                    </div>
+                    <div className={`pt-2 pb-4`}>
+                        {profileLinks.map((link) => {
+                            return (
+                                <a
+                                    class="block mt-2 text-lg hover:text-gray-500 transition"
+                                    href={link.location}
+                                >
+                                    {link.icon}
+                                    {link.name}
+                                </a>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
         </header>
     );
--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 export default function ProfileDropdown({ className }) {
-    const links = [
+    const [isOpen, setIsOpen] = useState(false);
+
+    const profileLinks = [
         {
             name: "Hires",
             location: "#",
@@ -28,12 +30,23 @@ export default function ProfileDropdown({ className }) {
         },
     ];
 
-    const [isOpen, setIsOpen] = useState(true);
+    const handleEscape = (e) => {
+        if (e.key === "Esc" || e.key === "Escape") {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleEscape, false);
+
+        return () =>
+            document.removeEventListener("keydown", handleEscape, false);
+    });
 
     return (
         <div className={`relative ${className}`}>
             <button
-                className="block w-12 h-12 rounded-full overflow-hidden border-2 border-gray-400 focus:outline-none focus:border-4 focus:border-indigo-500"
+                className="z-10 relative block w-12 h-12 rounded-full overflow-hidden border-2 border-gray-400 focus:outline-none focus:border-4 focus:border-indigo-500"
                 onClick={() => {
                     setIsOpen(!isOpen);
                 }}
@@ -44,12 +57,21 @@ export default function ProfileDropdown({ className }) {
                     alt="Avatar"
                 />
             </button>
+            <button
+                onClick={() => {
+                    setIsOpen(false);
+                }}
+                tabIndex="-1"
+                className={`bg-black h-full w-full fixed inset-0 opacity-30 cursor-default ${
+                    isOpen ? "" : "hidden"
+                }`}
+            ></button>
             <div
                 class={`absolute right-0 mt-2 w-56 bg-white py-2 rounded-lg shadow-lg ${
                     isOpen ? "" : "hidden"
                 }`}
             >
-                {links.map((link) => {
+                {profileLinks.map((link) => {
                     return (
                         <a
                             class="block px-4 py-2 font-medium hover:bg-indigo-500 focus:bg-indigo-500 hover:text-white focus:text-white transition-all"
