4f69f42883bd20ed5436ebb22e029d2f2c479e49
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-11-26 17:43:00 -0300

    feat: styles and state for search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,13 @@
 import { FiFilter, FiSearch } from 'react-icons/fi';
 
+import { useState } from 'react';
+
 export default function SearchBar() {
+    const [isFiltersMenuOpen, setIsFiltersMenuOpen] = useState(false);
+
     return (
-        <section className="p-5 bg-gray-800">
-            <div className="flex justify-between">
+        <section className="bg-gray-800">
+            <div className="flex justify-between p-5">
                 <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3">
                         <FiSearch className="w-6 h-6 text-gray-600" />
@@ -11,117 +15,175 @@ export default function SearchBar() {
                     <input
                         type="text"
                         placeholder="Search by keywords"
-                        className="w-full py-2 pl-10 pr-4 text-gray-300 transition bg-gray-900 rounded-lg focus:outline-none focus:bg-gray-200 focus:text-gray-900"
+                        className="w-full py-2 pl-10 pr-4 text-gray-300 transition bg-gray-900 border-transparent rounded-lg focus:outline-none focus:bg-gray-200 focus:text-gray-900"
                     />
                 </div>
-                <button className="inline-flex py-2 pl-3 pr-4 ml-2 bg-gray-700 rounded-lg shadow hover:bg-gray-600 active:bg-gray-800 focus:outline-none focus:ring">
+                <button
+                    className={`inline-flex py-2 pl-3 pr-4 ml-2 rounded-lg  shadow focus:ring ${
+                        isFiltersMenuOpen
+                            ? 'bg-gray-900 hover:bg-gray-600'
+                            : 'bg-gray-700 hover:bg-gray-600'
+                    }`}
+                    onClick={() => setIsFiltersMenuOpen(!isFiltersMenuOpen)}
+                >
                     <FiFilter className="w-6 h-6 text-gray-500" />
                     <span className="ml-1 font-medium text-white">Filters</span>
                 </button>
             </div>
-            <form>
-                <fieldset>
-                    <div>
-                        <label>
-                            <span>Bedrooms</span>
-                            <select name="bedroom_count" id="bedroom_count">
+            <form className={`${isFiltersMenuOpen ? 'block' : 'hidden'}`}>
+                <fieldset className="p-5 border-t border-gray-900">
+                    <div className="flex -mx-2">
+                        <label className="block w-1/2 px-2">
+                            <span className="text-sm text-gray-300">
+                                Bedrooms
+                            </span>
+                            <select className="block w-full py-2 mt-1 text-gray-300 bg-gray-700 border-transparent rounded-lg shadow focus:bg-gray-600 focus:outline-none ">
                                 <option value="1">1</option>
                             </select>
                         </label>
-                        <label>
-                            <span>Bathrooms</span>
-                            <select name="bathroom_count" id="bathroom_count">
+                        <label className="block w-1/2 px-2">
+                            <span className="text-sm text-gray-300">
+                                Bathrooms
+                            </span>
+                            <select className="block w-full py-2 mt-1 text-gray-300 bg-gray-700 border-transparent rounded-lg shadow focus:bg-gray-600 focus:outline-none">
                                 <option value="1">1</option>
                             </select>
                         </label>
                     </div>
-                    <div>
-                        <label>
-                            <span>Price range</span>
-                            <select name="price_range" id="price_range">
+                    <div className="mt-4">
+                        <label className="block w-full">
+                            <span className="text-sm text-gray-300">
+                                Price range
+                            </span>
+                            <select className="block w-full py-2 mt-1 text-gray-300 bg-gray-700 border-transparent rounded-lg shadow focus:bg-gray-600 focus:outline-none ">
                                 <option value="500">Up to $500 /wk</option>
                                 <option value="1000">Up to $1,000 /wk</option>
                             </select>
                         </label>
                     </div>
                 </fieldset>
-                <fieldset>
-                    <legend>Property type</legend>
-                    <label>
+                <fieldset className="p-5 border-t border-gray-900">
+                    <span className="text-sm text-gray-300">
+                        <legend>Property type</legend>
+                    </span>
+                    <label className="flex items-center mt-3">
                         <input
                             type="radio"
                             name="property_type"
                             value="house"
                             id="house"
+                            className="w-6 h-6 bg-gray-900 border-transparent focus:bg-gray-700"
                         />
-                        <label htmlFor="house">House</label>
+                        <span className="ml-3 text-white">House</span>
                     </label>
-                    <label>
+                    <label className="flex items-center mt-3">
                         <input
                             type="radio"
                             name="property_type"
                             value="apartment"
                             id="apartment"
+                            className="w-6 h-6 bg-gray-900 border-transparent focus:bg-gray-700"
                         />
-                        <label htmlFor="apartment">Apartment</label>
+                        <span className="ml-3 text-white">Apartment</span>
                     </label>
-                    <label>
+                    <label className="flex items-center mt-3">
                         <input
                             type="radio"
                             name="property_type"
                             value="loft"
                             id="loft"
+                            className="w-6 h-6 bg-gray-900 border-transparent focus:bg-gray-700"
                         />
-                        <label htmlFor="loft">Loft</label>
+                        <span className="ml-3 text-white">Loft</span>
+                    </label>
+                    <label className="flex items-center mt-3">
+                        <input
+                            type="radio"
+                            name="property_type"
+                            value="chalet"
+                            id="chalet"
+                            className="w-6 h-6 bg-gray-900 border-transparent focus:bg-gray-700"
+                        />
+                        <span className="ml-3 text-white">Chalet</span>
                     </label>
                 </fieldset>
-                <fieldset>
-                    <legend>Amenities</legend>
-                    <label>
-                        <input type="checkbox" name="balcony" id="balcony" />
-                        <label htmlFor="balcony">Balcony</label>
+                <fieldset className="p-5 border-t border-gray-900">
+                    <span className="text-sm text-gray-300">
+                        <legend>Amenities</legend>
+                    </span>
+                    <label className="flex items-center mt-3">
+                        <input
+                            type="checkbox"
+                            name="balcony"
+                            id="balcony"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
+                        />
+                        <span className="ml-3 text-white">Balcony</span>
                     </label>
-                    <label>
+                    <label className="flex items-center mt-3">
                         <input
                             type="checkbox"
                             name="air_conditioning"
                             id="air_conditioning"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
                         />
-                        <label htmlFor="air_conditioning">
+                        <span className="ml-3 text-white">
                             Air conditioning
-                        </label>
+                        </span>
                     </label>
-                    <label>
-                        <input type="checkbox" name="pool" id="pool" />
-                        <label htmlFor="pool">Pool</label>
+                    <label className="flex items-center mt-3">
+                        <input
+                            type="checkbox"
+                            name="pool"
+                            id="pool"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
+                        />
+                        <span className="ml-3 text-white">Pool</span>
                     </label>
-                    <label>
-                        <input type="checkbox" name="beach" id="beach" />
-                        <label htmlFor="beach">Beach</label>
+                    <label className="flex items-center mt-3">
+                        <input
+                            type="checkbox"
+                            name="beach"
+                            id="beach"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
+                        />
+                        <span className="ml-3 text-white">Beach</span>
                     </label>
-                    <label>
+                    <label className="flex items-center mt-3">
                         <input
                             type="checkbox"
                             name="pet_friendly"
                             id="pet_friendly"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
                         />
-                        <label htmlFor="pet_friendly">Pet friendly</label>
+                        <span className="ml-3 text-white">Pet friendly</span>
                     </label>
-                    <label>
+                    <label className="flex items-center mt-3">
                         <input
                             type="checkbox"
                             name="kid_friendly"
                             id="kid_friendly"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
                         />
-                        <label htmlFor="kid_friendly">Kid friendly</label>
+                        <span className="ml-3 text-white">Kid friendly</span>
                     </label>
-                    <label>
-                        <input type="checkbox" name="parking" id="parking" />
-                        <label htmlFor="parking">Parking</label>
+                    <label className="flex items-center mt-3">
+                        <input
+                            type="checkbox"
+                            name="parking"
+                            id="parking"
+                            className="w-6 h-6 bg-gray-900 border-transparent rounded-lg focus:bg-gray-700"
+                        />
+                        <span className="ml-3 text-white">Parking</span>
                     </label>
                 </fieldset>
-                <div>
-                    <button type="submit">Filter results</button>
+                <div className="p-5 bg-gray-900">
+                    <button
+                        type="submit"
+                        className="block w-full px-4 py-2 font-semibold text-white bg-blue-700 rounded-lg hover:bg-blue-600"
+                    >
+                        Filter results
+                    </button>
                 </div>
             </form>
         </section>
